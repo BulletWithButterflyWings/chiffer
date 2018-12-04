0d28991cfb69796b96f7a8516a6d015e5e1e37b6
Alfons Örtemyr <oral16@nti.umea>
2018-12-04 10:08:32 +0100

    Start av program

---
+++ chiffer.py
@@ -0,0 +1,14 @@
+word = "test"
+key = 12
+letters = []
+crypt = []
+
+for letters in word:
+    letters.append(ord(letter) + key
+    print(letters)
+
+for l in letters:
+    crypt.append(chr(l -key)
+    print(crypt)
+
+           
