318a19a99b79b2717c465cb7525730913cdcb06a
Alfons Örtemyr <oral16@nti.umea>
2018-12-04 10:47:46 +0100

    Syntax

--- chiffer.py
+++ chiffer.py
@@ -4,11 +4,12 @@ letters = []
 crypt = []
 
 for letters in word:
-    letters.append(ord(letter) + key
+    letters.append(ord(letter) + key)
+    
     print(letters)
 
 for l in letters:
     crypt.append(chr(l -key)
-    print(crypt)
 
-           
+print(crypt)
+
