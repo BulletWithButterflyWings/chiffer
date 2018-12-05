8e8f9a2f306f9a429346470088cf72dd4755ddf4
Alfons Örtemyr <oral16@nti.umea>
2018-12-05 13:22:00 +0100

    Början av meny för chiffer

--- chiffer.py
+++ chiffer.py
@@ -2,9 +2,25 @@ word = "test" #
 key = 12 #
 letters = [] #
 crypt = [] #
+choice = 0
 
-for letter in word: # En loop, tills hela ordet är klart. 
-    letters.append(ord(letter) + key) #
+while choice != 3:
+
+    print("Hej och välkomen till krypteringsprogrammet!")
+    print("Tryck 1 för kryptering.")
+    print("Tryck 2 för dekryptering.")
+    print("Tryck 3 för att avsluta.")
+
+    try:
+        choice = int(input("Gör ett val"))
+    except:
+        print("Du gjorde inte ett val, försök igen.")
+
+        
+
+if choice == 1:
+    for letter in word: # En loop, tills hela ordet är klart. 
+        letters.append(ord(letter) + key) #
     
     print(letters) #
 
